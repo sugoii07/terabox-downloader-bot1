9791eaa0bfa65c1b53aed9bd2ee5766982680c1b
sugoii07 <acctest10569@gmail.com>
2025-09-07 00:05:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,15 +9,15 @@ BOT_TOKEN = "8443954411:AAG_qf8NJjN6dqNpzGc9ppMHim2tTLa234k"  # bot token
 #PORT = 6379  # redis port
 #PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+#PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
-COOKIE = ""
-ADMINS = [1317173146]
+#COOKIE = ""
+#ADMINS = [1317173146]
 
 
 BOT_USERNAME = "teraboxdown_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+#FORCE_LINK = "@RoldexVerse"
 
-PUBLIC_EARN_API = ""  # https://publicearn.com/api
+#PUBLIC_EARN_API = ""  # https://publicearn.com/api
